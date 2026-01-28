3b2d5256ff5e6f344602427d477d249b864eed16
meedastitou <astitoumd@gmail.com>
2026-01-28 14:10:15 +0100

    ajouter __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -9,3 +9,4 @@ from app.routers.rfq import router as rfq_router
 from app.routers.reponses import router as reponses_router
 from app.routers.decision import router as decision_router
 from app.routers.bon_commande import router as bon_commande_router
+from app.routers.selections import router as selections_router
